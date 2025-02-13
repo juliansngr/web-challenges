85b26838018503a770e3169ebc3828e82439dde1
Julian <julian.andreduerr@web.de>
2025-02-13 12:05:54 +0100

    exercise solution

--- js-conditions-and-booleans/js-conditions-and-booleans_statements/js/index.js
+++ js-conditions-and-booleans/js-conditions-and-booleans_statements/js/index.js
@@ -5,22 +5,53 @@ const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
 const receivedPassword = "password1234";
 
+if (receivedPassword === SUPER_SECRET_PASSWORD) {
+  console.log("Welcome! You are logged in as Brunhilde1984.");
+} else {
+  console.log("Access denied!");
+}
+
 // Part 2: Even / Odd
-const number = 6;
+const number = 4;
+
+if (number % 2 === 0) {
+  console.log("Zahl ist gerade!");
+} else {
+  console.log("Zahl ist ungerade!");
+}
 
 // Part 3: Hotdogs
-const numberOfHotdogs = 42;
+const numberOfHotdogs = 1000000;
+let totalPrice;
+
+if (numberOfHotdogs < 5) {
+  console.log("Preis: 2,00€");
+  totalPrice = numberOfHotdogs * 2;
+  console.log(totalPrice);
+} else if (numberOfHotdogs >= 5 && numberOfHotdogs < 100) {
+  console.log("Preis: 1,50€");
+  totalPrice = numberOfHotdogs * 1.5;
+  console.log(totalPrice);
+} else if (numberOfHotdogs >= 100 && numberOfHotdogs < 1000000) {
+  console.log("Preis: 1,00€");
+  totalPrice = numberOfHotdogs * 1;
+  console.log(totalPrice);
+} else if (numberOfHotdogs >= 1000000) {
+  console.log("Preis: 0,10€");
+  totalPrice = numberOfHotdogs * 0.1;
+  console.log(totalPrice);
+}
 
 // Part 4: Daytime
-const currentHour = 12;
+const currentHour = 18;
 
-const statement = "";
+const statement = currentHour < 17 ? "Still need to learn..." : "Partytime!!!";
 
 console.log(statement);
 
 // Part 5: Greeting
-const userName = "Archibald";
+const userName = "Wilhelm";
 
-const greeting = "Hello " + "//enter your code here" + "!";
+const greeting = "Hello " + (userName === "Felix" ? "Coach" : userName) + "!";
 
 console.log(greeting);
