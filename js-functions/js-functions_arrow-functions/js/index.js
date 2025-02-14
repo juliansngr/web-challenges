76914624d1eea9ca03875fb16dcfdca408312f55
Julian <julian.andreduerr@web.de>
2025-02-14 16:25:17 +0100

    exercise solution

--- js-functions/js-functions_arrow-functions/js/index.js
+++ js-functions/js-functions_arrow-functions/js/index.js
@@ -7,7 +7,7 @@ Use implicit returns when possible.
 HINT: You can find more information about arrow functions and implicit returns in the handout.
 */
 
-function getCurrentHour() {
+const getCurrentHour = () => {
   const now = new Date();
   const currentHour = now.getHours();
   if (currentHour === 0) {
@@ -19,21 +19,39 @@ function getCurrentHour() {
   } else {
     return currentHour - 12 + "pm";
   }
-}
+};
 
-function getVectorLength(x, y, z) {
-  return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
-}
+// function getCurrentHour() {
+//   const now = new Date();
+//   const currentHour = now.getHours();
+//   if (currentHour === 0) {
+//     return "12am";
+//   } else if (currentHour === 12) {
+//     return "12pm";
+//   } else if (currentHour <= 11) {
+//     return currentHour + "am";
+//   } else {
+//     return currentHour - 12 + "pm";
+//   }
+// }
 
-function cleanInput(string) {
-  return string.toLowerCase().trim();
-}
+const getVectorLength = (x, y, z) => (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+
+// function getVectorLength(x, y, z) {
+//   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+// }
+
+const cleanInput = (string) => string.toLowerCase().trim();
+
+// function cleanInput(string) {
+//   return string.toLowerCase().trim();
+// }
 
 /*
 Rewrite the following arrow functions as classic functions.
 */
 
-const isOddAndSmall = (number) => {
+function isOddAndSmall(number) {
   if (number > 10) {
     return false;
   }
@@ -43,8 +61,28 @@ const isOddAndSmall = (number) => {
   }
 
   return true;
-};
+}
 
-const add3 = (a, b, c) => a + b + c;
+// const isOddAndSmall = (number) => {
+//   if (number > 10) {
+//     return false;
+//   }
 
-const repeat10 = (string) => string.repeat(10);
+//   if (number % 2 === 0) {
+//     return false;
+//   }
+
+//   return true;
+// };
+
+function add3(a, b, c) {
+  return a + b + c;
+}
+
+// const add3 = (a, b, c) => a + b + c;
+
+function repeat10(string) {
+  return string.repeat(10);
+}
+
+// const repeat10 = (string) => string.repeat(10);
