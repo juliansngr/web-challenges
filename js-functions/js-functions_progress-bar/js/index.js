cb1be2aba2a722a86bb1b6943a29f4d2a7739964
Julian <julian.andreduerr@web.de>
2025-02-14 16:53:39 +0100

    initial commit

--- js-functions/js-functions_progress-bar/js/index.js
+++ js-functions/js-functions_progress-bar/js/index.js
@@ -2,4 +2,13 @@ console.clear();
 
 const progressBar = document.querySelector('[data-js="progress-bar"]');
 
-function calculateScrollPercentage() {}
+document.addEventListener("scroll", () => {
+  progressBar.style.width = calculateScrollPercentage();
+});
+
+function calculateScrollPercentage() {
+  const scrollPercentage =
+    (window.scrollY / (document.body.clientHeight - window.innerHeight)) * 100 +
+    "%";
+  return scrollPercentage;
+}
