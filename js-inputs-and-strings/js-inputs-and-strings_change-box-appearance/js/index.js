40816bd992d989a3ebd9c9fca6696ac6c77c37f4
Julian <julian.andreduerr@web.de>
2025-02-17 15:53:41 +0100

    exercise solution

--- js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,21 @@
 console.clear();
+
+const colorInput = document.querySelector('[data-js="input-color"]');
+const borderInput = document.querySelector('[data-js="input-radius"]');
+const rotationInput = document.querySelector('[data-js="input-rotation"]');
+const boxElement = document.querySelector('[data-js="box"]');
+
+colorInput.addEventListener("input", () => {
+  colorValue = Number(colorInput.value);
+  boxElement.style.backgroundColor = `hsl(${colorValue} 100% 50%)`;
+});
+
+borderInput.addEventListener("input", () => {
+  borderValue = Number(borderInput.value);
+  boxElement.style.borderRadius = `${borderValue}%`;
+});
+
+rotationInput.addEventListener("input", () => {
+  rotationValue = Number(rotationInput.value);
+  boxElement.style.transform = `rotate(${rotationValue}deg)`;
+});
