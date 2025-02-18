bf9ea0270c43b6485d56f6f381d82ed5d610f126
Julian <julian.andreduerr@web.de>
2025-02-18 16:18:27 +0100

    exercise solution

--- js-createelement/js-createelement_product-teaser/js/index.js
+++ js-createelement/js-createelement_product-teaser/js/index.js
@@ -9,3 +9,46 @@ const category3 = "Plankton Diet";
 const price = "149,99 €";
 const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
+
+const newProduct = document.createElement("article");
+newProduct.classList.add("product");
+newProduct.innerHTML = `
+      <section class="product__body">
+        <div class="product__text-container">
+          <h2 class="product__name">${name}</h2>
+          <ul class="product__categories">
+            <li class="product__category">${category1}</li>
+            <li class="product__category">${category2}</li>
+            <li class="product__category">${category3}</li>
+          </ul>
+          <p class="product__description">
+            ${description}
+          </p>
+        </div>
+        <div class="product__image-container">
+          <img
+            class="product__image"
+            src=${imageSrc}
+            alt=""
+          />
+        </div>
+      </section>`;
+
+const productFooter = document.createElement("footer");
+productFooter.classList.add("product__footer");
+const priceSpan = document.createElement("span");
+priceSpan.classList.add("product__price");
+priceSpan.textContent = `${price}`;
+productFooter.append(priceSpan);
+const buyButton = document.createElement("button");
+buyButton.classList.add("product__buy-button");
+buyButton.setAttribute("type", "button");
+buyButton.setAttribute("name", "buyButton");
+buyButton.textContent = "Buy";
+buyButton.addEventListener("click", () => {
+  console.log(`${name}`, `${price}`);
+});
+productFooter.append(buyButton);
+
+newProduct.append(productFooter);
+document.body.append(newProduct);
