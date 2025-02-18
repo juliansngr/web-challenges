478ca67d34fc5013b8ff86ec784ef58391c9a78c
Julian <julian.andreduerr@web.de>
2025-02-18 11:36:07 +0100

    exercise solution

--- js-forms/js-forms_checkbox-input/js/index.js
+++ js-forms/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const formSuccess = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,13 +13,38 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+function hideSuccess() {
+  formSuccess.setAttribute("hidden", "");
+}
+
+function showSuccess() {
+  formSuccess.removeAttribute("hidden");
+}
+
+hideSuccess();
+hideTosError();
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // --v-- write your code here --v--
-
-  // --^-- write your code here --^--
-
-  // eslint-disable-next-line no-alert
+  if (tosCheckbox.checked === false) {
+    console.log("failed");
+    showTosError();
+    return;
+  }
+  console.log("success");
+  showSuccess();
   alert("Form submitted");
+});
+
+tosCheckbox.addEventListener("change", (event) => {
+  event.preventDefault();
+
+  if (tosCheckbox.checked === false) {
+    showTosError();
+    hideSuccess();
+    return;
+  }
+
+  hideTosError();
 });
