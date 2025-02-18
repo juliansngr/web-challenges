3b3a194480765d0235478aee3153fa079ce13482
Julian <julian.andreduerr@web.de>
2025-02-18 11:16:31 +0100

    exercise solution including bonus

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -4,12 +4,18 @@ const form = document.querySelector('[data-js="form"]');
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  const formElement = event.target;
 
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
 
+  console.log(data);
+
+  const ageBadnessSum =
+    Number(formElement.elements.badness.value) +
+    Number(formElement.elements.age.value);
+  console.log("age-badness-sum: ", ageBadnessSum);
+
   event.target.reset();
   event.target.elements.firstName.focus();
-
-  console.log(data);
 });
