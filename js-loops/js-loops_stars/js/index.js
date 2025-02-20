3e04ec4654524eedcfd1803c512588b3211101df
Julian <julian.andreduerr@web.de>
2025-02-20 11:44:05 +0100

    exercise solution

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,13 +2,23 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // Reset the star container before re-rendering stars
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
-
+  for (let i = 1; i <= 5; i++) {
+    const img = document.createElement("img");
+    if (filledStars >= i) {
+      img.setAttribute("src", "assets/star-filled.svg");
+    } else {
+      img.setAttribute("src", "assets/star-empty.svg");
+    }
+    img.addEventListener("click", () => {
+      renderStars(i);
+    });
+    starContainer.append(img);
+  }
   // --^-- write or modify code above this line --^--
 }
-
-renderStars();
+renderStars(0);
