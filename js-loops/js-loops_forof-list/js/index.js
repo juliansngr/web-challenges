900e65af0bcc162199f39028b78b8674048a24db
Julian <julian.andreduerr@web.de>
2025-02-20 10:50:36 +0100

    exercise solution

--- js-loops/js-loops_forof-list/js/index.js
+++ js-loops/js-loops_forof-list/js/index.js
@@ -17,4 +17,10 @@ const programmingLanguages = [
 
 // --v-- write or modify code below this line --v--
 
+for (const pL of programmingLanguages) {
+  const li = document.createElement("li");
+  li.textContent = `${pL}`;
+  ol.append(li);
+}
+
 // --^-- write or modify code above this line --^--
