82e74b113b9f57c99a141b2638d839fdfcca478d
Julian <julian.andreduerr@web.de>
2025-02-24 15:39:35 +0100

    exercise solution

---
+++ js-error-handling/js-error-handling_calculator/js/index.js
@@ -0,0 +1,29 @@
+console.clear();
+
+const form = document.querySelector("form");
+const output = form.querySelector("output");
+
+const operations = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => {
+    if (b === 0) {
+      throw new Error("Cannot divide by zero!");
+    }
+    return a / b;
+  },
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const firstNumber = Number(event.target.firstNumber.value);
+  const secondNumber = Number(event.target.secondNumber.value);
+  const operation = event.target.operation.value;
+  try {
+    output.innerText = operations[operation](firstNumber, secondNumber);
+  } catch (error) {
+    const errorElement = document.querySelector("[class=error]");
+    errorElement.textContent = `Error Junge! ${error}`;
+  }
+});
