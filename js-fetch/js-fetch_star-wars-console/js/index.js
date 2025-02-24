44ab4bb8f659c3f7aff752d55ba405a78c7e0ba4
Julian <julian.andreduerr@web.de>
2025-02-24 10:46:59 +0100

    exercise solution

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,12 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  console.log(data.results[1].name);
+  console.log("R2-D2 eye-color:", data.results[5].eye_color);
+}
 
 fetchData();
