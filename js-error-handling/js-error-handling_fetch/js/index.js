7a50f325d24f71d03eed1855f8c7bb9512298242
Julian <julian.andreduerr@web.de>
2025-02-24 17:23:53 +0100

    exercise solution

--- js-error-handling/js-error-handling_fetch/js/index.js
+++ js-error-handling/js-error-handling_fetch/js/index.js
@@ -7,10 +7,15 @@ const errorElement = document.querySelector("[data-js='error']");
 async function fetchUserData(url) {
   try {
     const response = await fetch(url);
-
+    const contentType = response.headers.get("content-type");
+    if (!response.ok) {
+      throw new Error(`Fetch Error, ${response.status}`);
+    } else if (!contentType.includes("json")) {
+      throw new Error(`Expected JSON but got ${contentType}`);
+    }
     return await response.json();
-  } catch (error) {
-    return { error: error.message };
+  } catch (x) {
+    return { error: x.message };
   }
 }
 
@@ -28,17 +33,27 @@ endpoints.forEach((endpoint) => {
 
   button.addEventListener("click", async () => {
     const result = await fetchUserData(endpoint.url);
+    console.log(result);
+
+    errorElement.textContent = result;
 
     if (result.error) {
+      console.log(result);
+
       errorElement.textContent = result.error;
       userElement.innerHTML = "No user data available.";
     } else {
-      const user = result.data;
-      userElement.innerHTML = `
+      try {
+        const user = result.data;
+        userElement.innerHTML = `
       <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}" class="user__image"/>
       <h2>${user.first_name} ${user.last_name}</h2>
       `;
-      errorElement.textContent = "";
+        errorElement.textContent = "";
+      } catch (error) {
+        errorElement.textContent = error;
+        userElement.innerHTML = "No user data available.";
+      }
     }
   });
 });
