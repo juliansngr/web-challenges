c32ca018958d63e8187fb3408735d9c375f46a11
Julian <julian.andreduerr@web.de>
2025-02-24 10:58:47 +0100

    exercise solution

--- js-fetch/js-fetch_star-wars/js/index.js
+++ js-fetch/js-fetch_star-wars/js/index.js
@@ -34,13 +34,20 @@ const EXAMPLE_DATA = {
 };
 
 // Create dom element for a card and append it to the root
-const firstCard = Card(EXAMPLE_DATA);
-renderElement(firstCard);
+// const firstCard = Card(EXAMPLE_DATA);
+// renderElement(firstCard);
 
 fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.py4e.com/api/people");
+  const data = await response.json();
+  const persons = data.results;
+
+  persons.forEach((person) => {
+    const cardElement = Card(person);
+    renderElement(cardElement);
+  });
 }
