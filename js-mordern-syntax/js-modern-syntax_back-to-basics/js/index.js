1c2e1d17491760ec27d6f8b7dc2da9065a9f66a8
Julian <julian.andreduerr@web.de>
2025-02-25 12:51:45 +0100

    exercise solution

--- js-mordern-syntax/js-modern-syntax_back-to-basics/js/index.js
+++ js-mordern-syntax/js-modern-syntax_back-to-basics/js/index.js
@@ -5,13 +5,19 @@ try reversing the challenge by rewriting this code without using these modern fe
 
 export const getNameAndCountry = ({ name, country }) => [name, country];
 
-export const getRelocatedCity = (
-  city1,
-  city2 = { name: "Berlin", country: "Germany" }
-) => {
-  const [, country] = getNameAndCountry(city2);
+export const getRelocatedCity = (city1, city2) => {
+  if (!city1) {
+    city1 = { name: "Barcelona", country: "Spain" };
+  }
+
+  if (!city2) {
+    city2 = { name: "Berlin", country: "Germany" };
+  }
+
+  const country = city2.country;
+
   return {
-    ...city1,
-    country,
+    name: city1.name,
+    country: country,
   };
 };
