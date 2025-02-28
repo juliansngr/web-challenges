5b1eac942da8bc3b6efef644246ca68724eebdff
Julian <julian.andreduerr@web.de>
2025-02-28 12:09:43 +0100

    exercise solution

--- react-basics/react-basics_hello-world-article/src/App.jsx
+++ react-basics/react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,19 @@
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>H1</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat aut id
+        cumque illo, modi ea voluptas veritatis quam dolorum quae labore,
+        quibusdam dolorem aperiam quos laudantium. Perspiciatis beatae amet
+        tempora.
+      </p>
+    </article>
+  );
 }
