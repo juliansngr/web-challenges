b47d60916fdce9d90db9c13746047e4d705046f2
Julian <julian.andreduerr@web.de>
2025-02-28 12:21:06 +0100

    exercise solution

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,18 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Heading</h2>
+      <label htmlFor="input">Input Label</label>
+      <input id="input" />
+      <a href="https://wikipedia.org" className="article__link">
+        Link
+      </a>
+    </article>
+  );
 }
