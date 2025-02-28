dfe71ea2a575068a6bfaafcb4c5332e7c46879d9
Julian <julian.andreduerr@web.de>
2025-02-28 16:19:36 +0100

    exercise solution

--- react-props/react-props_smiley/src/App.jsx
+++ react-props/react-props_smiley/src/App.jsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Smiley isHappy />;
+}
+
+function Smiley({ isHappy }) {
+  return <h1>{isHappy ? "😄" : "😭"}</h1>;
 }
