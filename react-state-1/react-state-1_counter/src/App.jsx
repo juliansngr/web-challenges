bf853b51ae7cad00f38c22bd830682ed05203bc2
Julian <julian.andreduerr@web.de>
2025-03-03 16:16:20 +0100

    exercise solution

--- react-state-1/react-state-1_counter/src/App.jsx
+++ react-state-1/react-state-1_counter/src/App.jsx
@@ -1,26 +1,25 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
+
+  function plusCount() {
+    setCount(count + 1);
+  }
+
+  function minusCount() {
+    setCount(count - 1);
+  }
 
   return (
     <div className="container">
       <h1>{count}</h1>
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={minusCount}>
           -
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={plusCount}>
           +
         </button>
       </div>
