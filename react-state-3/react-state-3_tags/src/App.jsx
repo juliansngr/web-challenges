1cde9572c5899f3491764035174fa3135cc76dd5
Julian <julian.andreduerr@web.de>
2025-03-05 12:12:57 +0100

    exercise solution

--- react-state-3/react-state-3_tags/src/App.jsx
+++ react-state-3/react-state-3_tags/src/App.jsx
@@ -6,10 +6,19 @@ import List from "./components/List";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddTag(tagData) {
+    setTags([...tags, tagData]);
+  }
+
+  function handleDeleteTag(tagToDelete) {
+    // console.log(tagToDelete);
+    setTags(tags.filter((tag) => tag !== tagToDelete));
+  }
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleDeleteTag} />
     </main>
   );
 }
