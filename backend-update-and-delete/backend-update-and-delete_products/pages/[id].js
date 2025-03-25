7d0f823377b5910c619eb1cdc2437ac8160cb476
Julian <julian.andreduerr@web.de>
2025-03-25 16:05:27 +0100

    exercise solution

--- backend-update-and-delete/backend-update-and-delete_products/pages/[id].js
+++ backend-update-and-delete/backend-update-and-delete_products/pages/[id].js
@@ -2,12 +2,47 @@ import useSWR from "swr";
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import StyledLink from "@/components/Link";
+import { useState } from "react";
+import ProductForm from "@/components/ProductForm";
 
 export default function Product() {
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleEditProduct(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
+
+  async function handleDeleteProduct() {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      await response.json();
+      router.push("/");
+    }
+
+    if (!response.ok) {
+      console.log(response.status);
+    }
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -24,6 +59,23 @@ export default function Product() {
       <p>
         Price: {data.price} {data.currency}
       </p>
+      <button
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        Edit
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          handleDeleteProduct();
+        }}
+      >
+        Delete
+      </button>
+      {isEditMode && <ProductForm onSubmit={handleEditProduct} />}
       <StyledLink href="/">Back to all</StyledLink>
     </ProductCard>
   );
--- backend-update-and-delete/backend-update-and-delete_products/pages/api/products/[id].js
+++ backend-update-and-delete/backend-update-and-delete_products/pages/api/products/[id].js
@@ -17,5 +17,20 @@ export default async function handler(request, response) {
     return;
   }
 
+  if (request.method === "PUT") {
+    const productData = request.body;
+    await Product.findByIdAndUpdate(id, productData);
+
+    response.status(200).json({ status: "Product successfully updated." });
+    return;
+  }
+
+  if (request.method === "DELETE") {
+    await Product.findByIdAndDelete(id);
+
+    response.status(200).json({ status: "Product successfully deleted." });
+    return;
+  }
+
   response.status(405).json({ status: "Method not allowed." });
 }
